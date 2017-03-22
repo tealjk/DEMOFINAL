c040f0f94de9f744a3a121002716deeee93193ce
587852 <junkim@C02S222EFVH8.local>
2017-03-22 03:10:18 -0400

    second

---
+++ Jhip/src/main/webapp/app/app.module.ts
@@ -0,0 +1,58 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { UnivSharedModule, UserRouteAccessService } from './shared';
+import { UnivHomeModule } from './home/home.module';
+import { UnivAdminModule } from './admin/admin.module';
+import { UnivAccountModule } from './account/account.module';
+import { UnivEntityModule } from './entities/entity.module';
+
+import { LayoutRoutingModule } from './layouts';
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        UnivSharedModule,
+        UnivHomeModule,
+        UnivAdminModule,
+        UnivAccountModule,
+        UnivEntityModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        { provide: Window, useValue: window },
+        { provide: Document, useValue: document },
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class UnivAppModule {}
